12384bd9ae753e64a0f17acc6fcbc20b2fb504f1
Antoine Aubé <aube.antoine@gmail.com>
2017-03-31 14:33:15 +0200

    removed useless comments

--- j2e/kitchen/src/main/java/fr/unice/polytech/isa/tcf/OrderProcessing.java
+++ j2e/kitchen/src/main/java/fr/unice/polytech/isa/tcf/OrderProcessing.java
@@ -4,9 +4,6 @@ import fr.unice.polytech.isa.tcf.entities.Order;
 
 import javax.ejb.Local;
 
-/**
- * Created by huang on 10/03/17.
- */
 @Local
 public interface OrderProcessing {
     void process(Order order);
--- j2e/kitchen/src/main/java/fr/unice/polytech/isa/tcf/Tracker.java
+++ j2e/kitchen/src/main/java/fr/unice/polytech/isa/tcf/Tracker.java
@@ -5,9 +5,6 @@ import fr.unice.polytech.isa.tcf.exceptions.UnknownOrderId;
 
 import javax.ejb.Local;
 
-/**
- * Created by huang on 10/03/17.
- */
 @Local
 public interface Tracker {
     OrderStatus status(String orderId) throws UnknownOrderId;
